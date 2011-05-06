fe50ae71658935ac82c16f70ff1b4e8053e883d9
Kevin Pollet <pollet.kevin@gmail.com>
2011-05-05 23:20:29 -0400

    Add all bean validation built-in constraints.

--- src/main/java/org/jboss/forge/validation/ConstraintPlugin.java
+++ src/main/java/org/jboss/forge/validation/ConstraintPlugin.java
@@ -26,20 +26,25 @@ import java.io.FileNotFoundException;
 import javax.inject.Inject;
 import javax.validation.constraints.AssertFalse;
 import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Future;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import org.jboss.forge.parser.java.Annotation;
 import org.jboss.forge.parser.java.Field;
 import org.jboss.forge.parser.java.JavaClass;
-import org.jboss.forge.parser.java.JavaSource;
 import org.jboss.forge.project.Project;
 import org.jboss.forge.project.facets.JavaSourceFacet;
 import org.jboss.forge.resources.Resource;
 import org.jboss.forge.resources.java.JavaFieldResource;
-import org.jboss.forge.resources.java.JavaMemberResource;
 import org.jboss.forge.resources.java.JavaMethodResource;
 import org.jboss.forge.resources.java.JavaResource;
 import org.jboss.forge.shell.Shell;
@@ -54,8 +59,6 @@ import org.jboss.forge.validation.completer.PropertyCompleter;
 import org.jboss.forge.validation.util.ResourceHelper;
 
 //TODO add groups support
-//TODO add class constraints
-//TODO add all bean validation built-in constraints
 //TODO constraint list
 //TODO constraint on method
 
@@ -123,7 +126,7 @@ public class ConstraintPlugin implements Plugin
 
     @Command(value = "Min")
     public void addMinConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
-                                 @Option(name = "minValue", required = true) long min,
+                                 @Option(name = "min", required = true) long min,
                                  @Option(name = "message") String message) throws FileNotFoundException
     {
         final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, Min.class);
@@ -136,7 +139,7 @@ public class ConstraintPlugin implements Plugin
 
     @Command(value = "Max")
     public void addMaxConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
-                                 @Option(name = "maxValue", required = true) long max,
+                                 @Option(name = "max", required = true) long max,
                                  @Option(name = "message") String message) throws FileNotFoundException
     {
         final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, Max.class);
@@ -145,6 +148,129 @@ public class ConstraintPlugin implements Plugin
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
         outputConstraintAdded(property, Max.class);
+    }
+
+
+    @Command(value = "DecimalMin")
+    public void addDecimalMinConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+                                        @Option(name = "min", required = true) String min,
+                                        @Option(name = "message") String message) throws FileNotFoundException
+    {
+        final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, DecimalMin.class);
+        addConstraintMessageTo(constraintAnnotation, message);
+        constraintAnnotation.setStringValue(min);
+        javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
+
+        outputConstraintAdded(property, DecimalMin.class);
+    }
+
+    @Command(value = "DecimalMax")
+    public void addDecimalMaxConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+                                        @Option(name = "max", required = true) String max,
+                                        @Option(name = "message") String message) throws FileNotFoundException
+    {
+        final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, DecimalMax.class);
+        addConstraintMessageTo(constraintAnnotation, message);
+        constraintAnnotation.setStringValue(max);
+        javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
+
+        outputConstraintAdded(property, DecimalMax.class);
+    }
+
+    @Command(value = "Size")
+    public void addSizeConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+                                  @Option(name = "min") Integer min,
+                                  @Option(name = "max") Integer max,
+                                  @Option(name = "message") String message) throws FileNotFoundException
+    {
+        final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, Size.class);
+        addConstraintMessageTo(constraintAnnotation, message);
+
+        if (min != null)
+        {
+            constraintAnnotation.setLiteralValue("min", String.valueOf(min));
+        }
+
+        if (max != null)
+        {
+            constraintAnnotation.setLiteralValue("max", String.valueOf(max));
+        }
+
+        javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
+
+        outputConstraintAdded(property, Size.class);
+    }
+
+    @Command(value = "Digits")
+    public void addDigitsConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+                                    @Option(name = "integer") int integer,
+                                    @Option(name = "fraction") int fraction,
+                                    @Option(name = "message") String message) throws FileNotFoundException
+    {
+        final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, Digits.class);
+        addConstraintMessageTo(constraintAnnotation, message);
+        constraintAnnotation.setLiteralValue("integer", String.valueOf(integer));
+        constraintAnnotation.setLiteralValue("fraction", String.valueOf(fraction));
+        javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
+
+        outputConstraintAdded(property, Digits.class);
+    }
+
+    @Command(value = "Past")
+    public void addPastConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+                                  @Option(name = "message") String message) throws FileNotFoundException
+    {
+        final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, Past.class);
+        addConstraintMessageTo(constraintAnnotation, message);
+        javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
+
+        outputConstraintAdded(property, Past.class);
+    }
+
+    @Command(value = "Future")
+    public void addFutureConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+                                    @Option(name = "message") String message) throws FileNotFoundException
+    {
+        final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, Future.class);
+        addConstraintMessageTo(constraintAnnotation, message);
+        javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
+
+        outputConstraintAdded(property, Future.class);
+    }
+
+    //TODO need a closer look to this command
+    @Command(value = "Pattern")
+    public void addPatternConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+                                     @Option(name = "regexp", required = true) String regexp,
+                                     @Option(name = "flags") Pattern.Flag[] flags,
+                                     @Option(name = "message") String message) throws FileNotFoundException
+    {
+        final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, Pattern.class);
+        addConstraintMessageTo(constraintAnnotation, message);
+        constraintAnnotation.setStringValue("regexp", regexp);
+
+        constraintAnnotation.getOrigin().addImport(Pattern.Flag.class);
+        final StringBuilder flagsLiteral = new StringBuilder();
+        flagsLiteral.append('{');
+        if (flags != null)
+        {
+            int i = 0;
+            for (Pattern.Flag oneFlag : flags)
+            {
+                flagsLiteral.append(oneFlag);
+                if (i < (flags.length - 1))
+                {
+                    flagsLiteral.append(",");
+                }
+                i++;
+            }
+        }
+        flagsLiteral.append('}');
+
+        constraintAnnotation.setStringValue("flags", flagsLiteral.toString());
+        javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
+
+        outputConstraintAdded(property, Pattern.class);
     }
 
     private Annotation<JavaClass> addConstraintAnnotation(String property, Class<? extends java.lang.annotation.Annotation> annotationClass) throws FileNotFoundException
@@ -175,6 +301,7 @@ public class ConstraintPlugin implements Plugin
         }
     }
 
+    //TODO a better output will be great
     private void outputConstraintAdded(String property, Class<? extends java.lang.annotation.Annotation> constraintClass)
     {
         final StringBuilder builder = new StringBuilder();
