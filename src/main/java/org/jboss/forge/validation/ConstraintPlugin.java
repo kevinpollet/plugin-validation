1d5b206816fe1e350f4a13f4fb84fe3e3335449f
Kevin Pollet <pollet.kevin@gmail.com>
2011-05-12 22:52:39 +0200

    Modify commands option name.

--- src/main/java/org/jboss/forge/validation/ConstraintPlugin.java
+++ src/main/java/org/jboss/forge/validation/ConstraintPlugin.java
@@ -79,59 +79,59 @@ public class ConstraintPlugin implements Plugin
     }
 
     @Command(value = "Valid")
-    public void addValidConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property) throws FileNotFoundException
+    public void addValidConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property) throws FileNotFoundException
     {
         final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, Valid.class);
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
-        outputConstraintAdded(property, Valid.class);
+        outputConstraintAdded(Valid.class);
     }
 
     @Command(value = "Null")
-    public void addNullConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+    public void addNullConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property,
                                   @Option(name = "message") String message) throws FileNotFoundException
     {
         final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, Null.class);
         addConstraintMessageTo(constraintAnnotation, message);
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
-        outputConstraintAdded(property, Null.class);
+        outputConstraintAdded(Null.class);
     }
 
     @Command(value = "NotNull")
-    public void addNotNullConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+    public void addNotNullConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property,
                                      @Option(name = "message") String message) throws FileNotFoundException
     {
         final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, NotNull.class);
         addConstraintMessageTo(constraintAnnotation, message);
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
-        outputConstraintAdded(property, NotNull.class);
+        outputConstraintAdded(NotNull.class);
     }
 
     @Command(value = "AssertTrue")
-    public void addAssertTrueConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+    public void addAssertTrueConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property,
                                         @Option(name = "message") String message) throws FileNotFoundException
     {
         final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, AssertTrue.class);
         addConstraintMessageTo(constraintAnnotation, message);
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
-        outputConstraintAdded(property, AssertTrue.class);
+        outputConstraintAdded(AssertTrue.class);
     }
 
     @Command(value = "AssertFalse")
-    public void addAssertFalseConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+    public void addAssertFalseConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property,
                                          @Option(name = "message") String message) throws FileNotFoundException
     {
         final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, AssertFalse.class);
         addConstraintMessageTo(constraintAnnotation, message);
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
-        outputConstraintAdded(property, AssertFalse.class);
+        outputConstraintAdded(AssertFalse.class);
     }
 
     @Command(value = "Min")
-    public void addMinConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+    public void addMinConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property,
                                  @Option(name = "min", required = true) long min,
                                  @Option(name = "message") String message) throws FileNotFoundException
     {
@@ -140,11 +140,11 @@ public class ConstraintPlugin implements Plugin
         constraintAnnotation.setLiteralValue(String.valueOf(min));
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
-        outputConstraintAdded(property, Min.class);
+        outputConstraintAdded(Min.class);
     }
 
     @Command(value = "Max")
-    public void addMaxConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+    public void addMaxConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property,
                                  @Option(name = "max", required = true) long max,
                                  @Option(name = "message") String message) throws FileNotFoundException
     {
@@ -153,12 +153,12 @@ public class ConstraintPlugin implements Plugin
         constraintAnnotation.setLiteralValue(String.valueOf(max));
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
-        outputConstraintAdded(property, Max.class);
+        outputConstraintAdded(Max.class);
     }
 
 
     @Command(value = "DecimalMin")
-    public void addDecimalMinConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+    public void addDecimalMinConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property,
                                         @Option(name = "min", required = true) String min,
                                         @Option(name = "message") String message) throws FileNotFoundException
     {
@@ -167,11 +167,11 @@ public class ConstraintPlugin implements Plugin
         constraintAnnotation.setStringValue(min);
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
-        outputConstraintAdded(property, DecimalMin.class);
+        outputConstraintAdded(DecimalMin.class);
     }
 
     @Command(value = "DecimalMax")
-    public void addDecimalMaxConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+    public void addDecimalMaxConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property,
                                         @Option(name = "max", required = true) String max,
                                         @Option(name = "message") String message) throws FileNotFoundException
     {
@@ -180,11 +180,11 @@ public class ConstraintPlugin implements Plugin
         constraintAnnotation.setStringValue(max);
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
-        outputConstraintAdded(property, DecimalMax.class);
+        outputConstraintAdded(DecimalMax.class);
     }
 
     @Command(value = "Size")
-    public void addSizeConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+    public void addSizeConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property,
                                   @Option(name = "min") Integer min,
                                   @Option(name = "max") Integer max,
                                   @Option(name = "message") String message) throws FileNotFoundException
@@ -204,11 +204,11 @@ public class ConstraintPlugin implements Plugin
 
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
-        outputConstraintAdded(property, Size.class);
+        outputConstraintAdded(Size.class);
     }
 
     @Command(value = "Digits")
-    public void addDigitsConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+    public void addDigitsConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property,
                                     @Option(name = "integer") int integer,
                                     @Option(name = "fraction") int fraction,
                                     @Option(name = "message") String message) throws FileNotFoundException
@@ -219,34 +219,33 @@ public class ConstraintPlugin implements Plugin
         constraintAnnotation.setLiteralValue("fraction", String.valueOf(fraction));
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
-        outputConstraintAdded(property, Digits.class);
+        outputConstraintAdded(Digits.class);
     }
 
     @Command(value = "Past")
-    public void addPastConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+    public void addPastConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property,
                                   @Option(name = "message") String message) throws FileNotFoundException
     {
         final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, Past.class);
         addConstraintMessageTo(constraintAnnotation, message);
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
-        outputConstraintAdded(property, Past.class);
+        outputConstraintAdded(Past.class);
     }
 
     @Command(value = "Future")
-    public void addFutureConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+    public void addFutureConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property,
                                     @Option(name = "message") String message) throws FileNotFoundException
     {
         final Annotation<JavaClass> constraintAnnotation = addConstraintAnnotation(property, Future.class);
         addConstraintMessageTo(constraintAnnotation, message);
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
-        outputConstraintAdded(property, Future.class);
+        outputConstraintAdded(Future.class);
     }
 
-    //TODO need a closer look to this command
     @Command(value = "Pattern")
-    public void addPatternConstraint(@Option(name = "on", completer = PropertyCompleter.class) String property,
+    public void addPatternConstraint(@Option(name = "onProperty", completer = PropertyCompleter.class) String property,
                                      @Option(name = "regexp", required = true) String regexp,
                                      @Option(name = "flags") Pattern.Flag[] flags,
                                      @Option(name = "message") String message) throws FileNotFoundException
@@ -276,7 +275,7 @@ public class ConstraintPlugin implements Plugin
         constraintAnnotation.setStringValue("flags", flagsLiteral.toString());
         javaSourceFacet.saveJavaSource(constraintAnnotation.getOrigin());
 
-        outputConstraintAdded(property, Pattern.class);
+        outputConstraintAdded(Pattern.class);
     }
 
     private Annotation<JavaClass> addConstraintAnnotation(String property, Class<? extends java.lang.annotation.Annotation> annotationClass) throws FileNotFoundException
@@ -302,12 +301,8 @@ public class ConstraintPlugin implements Plugin
         }
     }
 
-    //TODO a better output will be great
-    private void outputConstraintAdded(String property, Class<? extends java.lang.annotation.Annotation> constraintClass)
+    private void outputConstraintAdded(Class<? extends java.lang.annotation.Annotation> constraintClass)
     {
-        final StringBuilder builder = new StringBuilder();
-        builder.append(constraintClass.getSimpleName() + " has been added on ");
-        builder.append(property != null ? property : shell.getCurrentResource().getName());
-        shell.println(builder.toString());
+        shell.println("Constraint " + constraintClass.getSimpleName() + " has been successfully added.");
     }
 }
